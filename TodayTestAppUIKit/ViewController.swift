74556cf69a6d7320e55ba2f50c58a4a278cf909f
KirTarasko <kir.tarasko@gmail.com>
2022-11-22 22:12:00 +0200

    Initial Commit

---
+++ TodayTestAppUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodayTestAppUIKit
+//
+//  Created by Kirill Tarasko on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
