d7cfbdeab449a332d4fdea7aded23d083f78a9a6
KirTarasko <tarasko.kirill@icloud.com>
2022-11-24 19:20:37 +0200

    Test

--- TodayTestAppUIKit/Models/Reminder.swift
+++ TodayTestAppUIKit/Models/Reminder.swift
@@ -19,7 +19,6 @@ extension Reminder {
     static var sampleData = [
         Reminder(title: "Английскому язык", dueDate: Date().addingTimeInterval(2000), notes: "Изучение английского языка по приложению Duolingo"),
         Reminder(title: "Программирование", dueDate: Date().addingTimeInterval(10000), notes: "Изучение языка Swift"),
-        Reminder(title: "Португальскому язык", dueDate: Date().addingTimeInterval(20000), notes: "Изучение португальского языка по приложению Duolingo"),
         Reminder(title: "Португальскому язык", dueDate: Date().addingTimeInterval(20000), notes: "Изучение португальского языка по приложению Duolingo")
     ]
 }
