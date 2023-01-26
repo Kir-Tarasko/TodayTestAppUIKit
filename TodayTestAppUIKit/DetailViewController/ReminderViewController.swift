57bf9af3afe33b50060a20206594ff5cb3e9d382
KirTarasko <tarasko.kirill@icloud.com>
2023-01-26 21:01:56 +0200

    Cancel edits button

--- TodayTestAppUIKit/DetailViewController/ReminderViewController.swift
+++ TodayTestAppUIKit/DetailViewController/ReminderViewController.swift
@@ -71,7 +71,13 @@ class ReminderViewController: UICollectionViewController {
         cell.tintColor = .todayPrimaryTint
     }
     
+    @objc func didCancelEdit() {
+        workingReminder = reminder
+        setEditing(false, animated: true)
+    }
+    
     private func prepareForEditing() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(didCancelEdit))
         updateSnapshotForEditing()
     }
     
@@ -85,6 +91,7 @@ class ReminderViewController: UICollectionViewController {
     }
     
     private func prepareForViewing() {
+        navigationItem.leftBarButtonItem = nil
         if workingReminder != reminder {
             reminder = workingReminder
         }
