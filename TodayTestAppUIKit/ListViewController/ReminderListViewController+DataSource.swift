45aff6c3df86c10e4afa1db972a8d40e1f8c8ba5
KirTarasko <tarasko.kirill@icloud.com>
2023-02-01 21:18:19 +0200

    Delete a reminder

--- TodayTestAppUIKit/ListViewController/ReminderListViewController+DataSource.swift
+++ TodayTestAppUIKit/ListViewController/ReminderListViewController+DataSource.swift
@@ -58,6 +58,11 @@ extension ReminderListViewController {
         reminders.append(reminder)
     }
     
+    func deleteReminder(withId id: Reminder.ID) {
+        let index = reminders.indexOfReminder(with: id)
+        reminders.remove(at: index)
+    }
+    
     private func doneButtonAccessibilityAction(for reminder: Reminder) -> UIAccessibilityCustomAction {
         let name = NSLocalizedString("Toggle completion", comment: "Reminder done button accessibility label")
         let action = UIAccessibilityCustomAction(name: name) { [weak self] action in
