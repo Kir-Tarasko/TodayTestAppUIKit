e1c2cf5ad56d114e655f21f6322cf44c3b948358
KirTarasko <tarasko.kirill@icloud.com>
2023-02-06 20:54:50 +0200

    Update progress dynamically

--- TodayTestAppUIKit/ListViewController/ReminderListViewController.swift
+++ TodayTestAppUIKit/ListViewController/ReminderListViewController.swift
@@ -21,6 +21,14 @@ class ReminderListViewController: UICollectionViewController {
         ReminderListStyle.today.name, ReminderListStyle.future.name, ReminderListStyle.all.name
     ])
     var headerView: ProgressHeaderView?
+        var progress: CGFloat {
+            let chunkSize = 1.0 / CGFloat(filteredReminders.count)
+            let progress = filteredReminders.reduce(0.0) {
+                let chunk = $1.isComplete ? chunkSize : 0
+                return $0 + chunk
+            }
+            return progress
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,6 +73,15 @@ class ReminderListViewController: UICollectionViewController {
         let id = filteredReminders[indexPath.item].id
         pushDetailViewForReminder(withId: id)
         return false
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, willDisplaySupplementaryView view: UICollectionReusableView, forElementKind elementKind: String, at indexPath: IndexPath) {
+        guard elementKind == ProgressHeaderView.elementKind,
+              let progressView = view as? ProgressHeaderView
+        else {
+            return
+        }
+        progressView.progress = progress
     }
     
     func pushDetailViewForReminder(withId id: Reminder.ID) {
