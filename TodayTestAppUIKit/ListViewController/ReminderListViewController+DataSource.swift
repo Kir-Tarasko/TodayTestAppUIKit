3a0cd9e5f486002e60ba6020421c59b600f44444
KirTarasko <tarasko.kirill@icloud.com>
2023-02-09 19:21:06 +0200

    Save reminders individually, save added and updated reminders,  delete reminders

--- TodayTestAppUIKit/ListViewController/ReminderListViewController+DataSource.swift
+++ TodayTestAppUIKit/ListViewController/ReminderListViewController+DataSource.swift
@@ -56,10 +56,16 @@ extension ReminderListViewController {
         let index = reminders.indexOfReminder(withId: id)
         return reminders[index]
     }
-
+    
     func updateReminder(_ reminder: Reminder) {
-        let index = reminders.indexOfReminder(withId: reminder.id)
-        reminders[index] = reminder
+        do {
+            try reminderStore.save(reminder)
+            let index = reminders.indexOfReminder(withId: reminder.id)
+            reminders[index] = reminder
+        } catch TodayError.accessDenied {
+        } catch {
+            showError(error)
+        }
     }
     
     func completeReminder(withId id: Reminder.ID) {
@@ -70,12 +76,26 @@ extension ReminderListViewController {
     }
     
     func addReminder(_ reminder: Reminder) {
-        reminders.append(reminder)
+        var reminder = reminder
+        do {
+            let idFromStore = try reminderStore.save(reminder)
+            reminder.id = idFromStore
+            reminders.append(reminder)
+        } catch TodayError.accessDenied {
+        } catch {
+            showError(error)
+        }
     }
     
     func deleteReminder(withId id: Reminder.ID) {
-        let index = reminders.indexOfReminder(withId: id)
-        reminders.remove(at: index)
+        do {
+            try reminderStore.remove(with: id)
+            let index = reminders.indexOfReminder(withId: id)
+            reminders.remove(at: index)
+        } catch TodayError.accessDenied {
+        } catch {
+            showError(error)
+        }
     }
     
     func prepareReminderStore() {
@@ -85,9 +105,9 @@ extension ReminderListViewController {
                 reminders = try await reminderStore.readAll()
                 NotificationCenter.default.addObserver(self, selector: #selector(eventStoreChanged(_:)), name: .EKEventStoreChanged, object: nil)
             } catch TodayError.accessDenied, TodayError.accessRestricted {
-                #if DEBUG
+#if DEBUG
                 reminders = Reminder.sampleData
-                #endif
+#endif
             } catch {
                 showError(error)
             }
--- TodayTestAppUIKit/Models/ReminderStore.swift
+++ TodayTestAppUIKit/Models/ReminderStore.swift
@@ -18,23 +18,23 @@ final class ReminderStore {
     }
     
     func requestAccess() async throws {
-            let status = EKEventStore.authorizationStatus(for: .reminder)
-            switch status {
-            case .authorized:
-                return
-            case .restricted:
-                throw TodayError.accessRestricted
-            case .notDetermined:
-                let accessGranted = try await ekStore.requestAccess(to: .reminder)
-                guard accessGranted else {
-                    throw TodayError.accessDenied
-                }
-            case .denied:
+        let status = EKEventStore.authorizationStatus(for: .reminder)
+        switch status {
+        case .authorized:
+            return
+        case .restricted:
+            throw TodayError.accessRestricted
+        case .notDetermined:
+            let accessGranted = try await ekStore.requestAccess(to: .reminder)
+            guard accessGranted else {
                 throw TodayError.accessDenied
-            @unknown default:
-                throw TodayError.unknown
             }
+        case .denied:
+            throw TodayError.accessDenied
+        @unknown default:
+            throw TodayError.unknown
         }
+    }
     
     func readAll() async throws -> [Reminder] {
         guard isAvailable else {
@@ -52,6 +52,30 @@ final class ReminderStore {
         }
         return reminders
     }
+    
+    @discardableResult
+    func save(_ reminder: Reminder) throws -> Reminder.ID {
+        guard isAvailable else {
+            throw TodayError.accessDenied
+        }
+        let ekReminder: EKReminder
+        do {
+            ekReminder = try read(with: reminder.id)
+        } catch {
+            ekReminder = EKReminder(eventStore: ekStore)
+        }
+        ekReminder.update(using: reminder, in: ekStore)
+        try ekStore.save(ekReminder, commit: true)
+        return ekReminder.calendarItemIdentifier
+    }
+    
+    func remove(with id: Reminder.ID) throws {
+            guard isAvailable else {
+                throw TodayError.accessDenied
+            }
+            let ekReminder = try read(with: id)
+            try ekStore.remove(ekReminder, commit: true)
+        }
     
     private func read(with id: Reminder.ID) throws -> EKReminder {
         guard let ekReminder = ekStore.calendarItem(withIdentifier: id) as? EKReminder else {
